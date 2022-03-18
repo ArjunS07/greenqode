db57227302ee42fb529f5031ef2f758fc8716f80
Arjun Sharma <arjun.sharma07@outlook.com>
2022-03-18 21:21:31 +0530

    try to fix migrations issue

--- qrcodes/settings.py
+++ qrcodes/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -173,4 +173,4 @@ sentry_sdk.init(
     send_default_pii=True
 )
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
