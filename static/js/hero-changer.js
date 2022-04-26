a53f969ae37e6d35a14407129ee948dcb74affdc
Arjun Sharma <arjun.sharma07@outlook.com>
2022-04-26 22:53:29 +0530

    fix hero

--- static/js/hero-changer.js
+++ static/js/hero-changer.js
@@ -1,36 +1,36 @@
 
 textList = [
-  "park's",
-  "residential complex's",
-  "campus'",
-  "resort's",
-  "hotel's",
+  "park",
+    "residential complex",
+
+  "college",
+  "campus",
+  "resort",
+  "hotel",
+  "school",
+
+  
 ];
 
 const cycle = document.querySelector("#changing-text");
-const biodiversity = document.querySelector("#biodiversity");
 
 let i = 0;
 const cycleText = () => {
 
-    
+
 
     cycle.className = "fadeOut";
-    biodiversity.className = "fadeOut";
 
     // will fade in after 1.5s = length of fadeout animation
     setTimeout(function () {
     cycle.className = "";
     cycle.className = "fadeIn";
 
-    biodiversity.className = "";
-    biodiversity.className = "fadeIn";
 
-    cycle.innerHTML = ""; biodiversity.innerHTML = "";
+    cycle.innerHTML = ""; 
 
     cycle.innerHTML = textList[i];
-    biodiversity.innerHTML = " biodiversity";
-  }, 1600);
+  }, 500);
 
   i = ++i % textList.length;
 };
@@ -39,6 +39,13 @@ const cycleText = () => {
 // biodiversity.className = "fadeIn";
 // cycle.innerHTML = textList[i];
 // biodiversity.innerHTML = "biodiversity";
+setTimeout(function () {
+    cycleText.call();
+
+}, 1000)
+
+setTimeout(function() {
+    setInterval(cycleText, 2000);
+}, 1000)
 
 
-setInterval(cycleText, 4000);
