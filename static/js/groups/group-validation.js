fbbf194777e6c9025c63ed197918373ec7ab1cd6
Arjun Sharma <arjun.sharma07@outlook.com>
2022-06-14 18:51:17 +0530

    UI fixes for group fields * Fix padding on map column * Prevent submission on enter key

--- static/js/groups/group-validation.js
+++ static/js/groups/group-validation.js
@@ -1,28 +1,33 @@
-let form = document.getElementById('creationForm');
-form.addEventListener('change', validateForm);
+let form = document.getElementById("creationForm");
+form.addEventListener("change", validateForm);
 
 function validateForm() {
-    let name = document.getElementById('groupName').value;
-    let quantityInputFields = document.getElementsByClassName('item-quantity-input');
-    let submitBtn = document.getElementById('submitbtn');
+  let name = document.getElementById("groupName").value;
+  let quantityInputFields = document.getElementsByClassName(
+    "item-quantity-input"
+  );
+  let submitBtn = document.getElementById("submitbtn");
 
-
-    var isOnlyZeros = true;
-    for (var i = 0; i < quantityInputFields.length; i++) {
-        if (quantityInputFields[i].value != 0) {
-            isOnlyZeros = false;
-            break;
-        }
+  var isOnlyZeros = true;
+  for (var i = 0; i < quantityInputFields.length; i++) {
+    if (quantityInputFields[i].value != 0) {
+      isOnlyZeros = false;
+      break;
     }
+  }
 
-    var hasValidInput = false;
-    if (name != '' && !isOnlyZeros) {
-        hasValidInput = true;
-    }
+  var hasValidInput = false;
+  if (name != "" && !isOnlyZeros) {
+    hasValidInput = true;
+  }
 
-    if (hasValidInput) {
-        submitBtn.disabled = false;
-    } else {
-        submitBtn.disabled = true;
-    }
+  if (hasValidInput) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
 }
+
+$("form").keyup(function (e) {
+  return e.which !== 13;
+});
