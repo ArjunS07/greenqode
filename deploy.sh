87bfa571ab37db0a94437e21e4fa0395d5c6d696
Arjun Sharma <arjun.sharma07@outlook.com>
2022-06-14 18:03:00 +0530

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+python3 manage.py collectstatic
+git add .
+git commit -m "update static files"
+git push heroku master
+heroku run python3 manage.py makemigrations
+heroku run python3 manage.py migrate
+heroku open
